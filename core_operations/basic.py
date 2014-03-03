ff123d41d93f8386e7ccb5f62a82ba1292949b89
Jason Hobbs <jason.hobbs@gmail.com>
2014-03-02 21:59:56 -0600

    core operations stuff

---
+++ core_operations/basic.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('messi5.jpg')
+cv2.namedWindow('img')
+
+pixel = img[100, 200]
+print(pixel)
+
+print(img.shape)
+
+ball = img[280:340, 330:390]
+img[273:333, 100:160] = ball
+img[:,:,2] -= 5
+
+cv2.namedWindow('img')
+
+while(1):
+    cv2.imshow('img', img)
+    if cv2.waitKey(20) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
